1b123bb07f0a1cd1977ed31567476f07e640aba3
Carlos Batista <cbatista8a@gmail.com>
2024-07-24 00:44:28 +0200

    refactor: improve ArtifactHandler test

--- tests/Application/ArtifactHandlerTest.php
+++ tests/Application/ArtifactHandlerTest.php
@@ -10,6 +10,7 @@ use CubaDevOps\Upgrader\Domain\Exceptions\ArtifactNotInstallableException;
 use CubaDevOps\Upgrader\Domain\Exceptions\DirectoryNotExistsException;
 use CubaDevOps\Upgrader\Domain\ValueObjects\Release;
 use CubaDevOps\Upgrader\Test\TestDoubles\Application\ArtifactHandlerDouble;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class ArtifactHandlerTest extends TestCase
@@ -19,14 +20,17 @@ class ArtifactHandlerTest extends TestCase
     private const INSTALLATION_PATH = '/tmp/upgrader';
     public const EXCLUDED_DIR = '.github/';
     public const EXCLUDED_FILE = 'LICENSE';
+    /**
+     * @var Release|(Release&object&MockObject)|(Release&MockObject)|(object&MockObject)|MockObject
+     */
+    private $release;
 
     /**
      * @throws ArtifactNotDownloadableException
      */
     public function testArtifactWasDownloaded(): void
     {
-        $this->release->method('getArtifactUrl')->willReturn(self::ARTIFACT_URL);
-        static::assertTrue($this->artifact_handler->download(self::DOWNLOAD_PATH));
+        static::assertTrue($this->artifact_handler->download(self::ARTIFACT_URL, self::DOWNLOAD_PATH));
         static::assertFileExists(self::DOWNLOAD_PATH);
     }
 
@@ -37,8 +41,7 @@ class ArtifactHandlerTest extends TestCase
     {
         $this->expectException(ArtifactNotDownloadableException::class);
         $this->expectExceptionMessage('The artifact could not be downloaded');
-        $this->release->method('getArtifactUrl')->willReturn('https://wrong-artifact.url');
-        $this->artifact_handler->download(self::DOWNLOAD_PATH);
+        $this->artifact_handler->download('https://wrong-artifact.url', self::DOWNLOAD_PATH);
     }
 
     /**
@@ -49,7 +52,7 @@ class ArtifactHandlerTest extends TestCase
         $this->expectException(ArtifactNotDownloadableException::class);
         $this->expectExceptionMessage('The artifact could not be saved to /wrong/path.zip');
         $this->release->method('getArtifactUrl')->willReturn(self::ARTIFACT_URL);
-        $this->artifact_handler->download('/wrong/path.zip');
+        $this->artifact_handler->download(self::ARTIFACT_URL, '/wrong/path.zip');
     }
 
     /**
@@ -61,8 +64,7 @@ class ArtifactHandlerTest extends TestCase
      */
     public function testArtifactWasInstalled(): void
     {
-        $this->release->method('getArtifactUrl')->willReturn(self::ARTIFACT_URL);
-        $this->artifact_handler->download(self::DOWNLOAD_PATH);
+        $this->artifact_handler->download(self::ARTIFACT_URL, self::DOWNLOAD_PATH);
         $installed = $this->artifact_handler->install(self::DOWNLOAD_PATH, self::INSTALLATION_PATH);
         static::assertTrue($installed);
         static::assertDirectoryExists(self::INSTALLATION_PATH);
@@ -85,8 +87,7 @@ class ArtifactHandlerTest extends TestCase
     {
         $this->expectException(ArtifactNotInstallableException::class);
         $this->expectExceptionMessage('The artifact could not be opened');
-        $this->release->method('getArtifactUrl')->willReturn(self::ARTIFACT_URL);
-        $this->artifact_handler->download(self::DOWNLOAD_PATH);
+        $this->artifact_handler->download(self::ARTIFACT_URL, self::DOWNLOAD_PATH);
         file_put_contents(self::DOWNLOAD_PATH, 'invalid-zip-content');
         $this->artifact_handler->install(self::DOWNLOAD_PATH, self::INSTALLATION_PATH);
     }
@@ -100,17 +101,44 @@ class ArtifactHandlerTest extends TestCase
     {
         $excluded_resources = [self::EXCLUDED_DIR, self::EXCLUDED_FILE];
         $this->release->method('getArtifactUrl')->willReturn(self::ARTIFACT_URL);
-        $this->artifact_handler = new ArtifactHandlerDouble($this->release);
-        $this->artifact_handler->download(self::DOWNLOAD_PATH);
+        $this->artifact_handler = new ArtifactHandlerDouble();
+        $this->artifact_handler->download($this->release->getArtifactUrl(), self::DOWNLOAD_PATH);
         $installed = $this->artifact_handler->install(self::DOWNLOAD_PATH, self::INSTALLATION_PATH, $excluded_resources);
         static::assertTrue($installed);
         static::assertNotEquals($this->artifact_handler->initial_zip_files_count, $this->artifact_handler->final_zip_files_count);
     }
 
+    /**
+     * @throws ArtifactNotInstallableException
+     * @throws DirectoryNotExistsException
+     * @throws ArtifactNotDownloadableException
+     */
+    public function artifactInstallSucceedsWithExcludedResources(): void
+    {
+        $excluded_resources = [self::EXCLUDED_DIR, self::EXCLUDED_FILE];
+        $this->artifact_handler->download(self::ARTIFACT_URL, self::DOWNLOAD_PATH);
+        $installed = $this->artifact_handler->install(self::DOWNLOAD_PATH, self::INSTALLATION_PATH, $excluded_resources);
+        static::assertTrue($installed);
+        static::assertDirectoryDoesNotExist(self::INSTALLATION_PATH.'/'.self::EXCLUDED_DIR);
+        static::assertFileDoesNotExist(self::INSTALLATION_PATH.'/'.self::EXCLUDED_FILE);
+    }
+
+    /**
+     * @throws ArtifactNotInstallableException
+     * @throws ArtifactNotDownloadableException
+     */
+    public function testArtifactInstallFailsWithInvalidInstallationPath(): void
+    {
+        $this->expectException(DirectoryNotExistsException::class);
+        $this->expectExceptionMessage('Directory "/invalid/installation/path" was not created');
+        $this->artifact_handler->download(self::ARTIFACT_URL, self::DOWNLOAD_PATH);
+        $this->artifact_handler->install(self::DOWNLOAD_PATH, '/invalid/installation/path');
+    }
+
     protected function setUp(): void
     {
         $this->release = $this->createMock(Release::class);
-        $this->artifact_handler = new ArtifactHandler($this->release);
+        $this->artifact_handler = new ArtifactHandler();
         @unlink(self::DOWNLOAD_PATH); // Ensure the file does not exist
         @unlink(self::INSTALLATION_PATH); // Ensure the dir does not exist
     }
